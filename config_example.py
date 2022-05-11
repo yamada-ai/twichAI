c82e96df5ddf23a15894e8eed2308681bfa11da0
yamada_ai <yamadanlp@gmail.com>
2022-05-12 02:15:43 +0900

    add config_example

---
+++ config_example.py
@@ -0,0 +1,5 @@
+TMI_TOKEN = '####'       # メモしたoauth:~のアレ
+CLIENT_ID = '####'    # メモしたクライアントID
+BOT_NICK = 'yamadaApp'      # Botのアカウント名
+BOT_PREFIX = '!'    # この文字を作成したコマンドの頭に付けることでコマンドと認識してくれます
+CHANNEL = ['yamada_ai_'] # 実際に配信をするアカウント名
