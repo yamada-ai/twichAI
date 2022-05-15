e8ffa82feb4ce24f4ee943e052ab5336d02f5656
yamada_ai <yamadanlp@gmail.com>
2022-05-15 16:06:22 +0900

    executable novel collector is added and add lib for collect novels

---
+++ train/data_process/format_novel.py
@@ -0,0 +1,105 @@
+import os
+import sys
+sys.path.append("../")
+from datatools.analyzer import *
+
+
+novel_path = "../../corpus/novel/"
+ncodes= [x  for x in os.listdir(novel_path) if "." not in x ]
+
+def format_EOS(sentence):
+    if sentence[-1] in ["。", "?", "!", "．"]:
+        return sentence
+    else:
+         return sentence+"。"
+
+def is_valid_utterance_segment(text:str)->bool:
+    # 複数の"「"には囲まれていない
+    if text.count("「") == 1 and text.count("」") == 1 and text[0]=="「" and text[-1]=="」":
+        return True
+    return False
+
+def clean_text_plain(text):
+    text_ = neologdn.normalize(text)
+    # text_ = re.sub(r'\([^\)]*\)', "", text_)
+    # text_ = re.sub(r'\([^\)]*\)', "", text_)
+    text_ = re.sub(r'\d+', "0", text_)
+    if "……" in text_:
+        text_ = text_.replace("……", "…")
+    return text_
+
+# 1ページの小説をデータに変換
+import numpy as np
+import copy
+pattern = '(?<=\「).*(?=\」)'
+def novel2data(novel:list):
+    data_list = []
+    """
+    1 : 会話
+    0 : それ以外
+
+    011, 101, 111 の時に登録可能とする
+    
+    """
+    # 1, 0 の登録
+    # zero_one_vector = np.zeros(len(novel))
+    zero_one = ""
+    novel_normalized = []
+
+    conv_data = []
+
+    for i, line in enumerate(novel):
+        # 普通の発話なら1
+        if is_valid_utterance_segment(line):
+            zero_one += "1"
+            utt = re.search(pattern, line).group()
+            # そもそも存在しないのであれば
+            if utt=="":
+                # print(line)
+                continue
+            utt = clean_text_plain(utt)
+            if utt=="":
+                utt = re.search(pattern, line).group()
+            utt = format_EOS( utt )
+            novel_normalized.append(utt)
+        else:
+            zero_one += "0"
+            novel_normalized.append(clean_text_plain(line))
+
+    descriptive = []
+    # 部分を検索
+    for i in range(2, len(novel_normalized)):
+        if zero_one[i]=="0":
+            descriptive.append(novel_normalized[i])
+        comp = zero_one[i-3:i]
+        if "011" == comp:
+            conv_data.append(descriptive[-3:] + novel_normalized[i-2:i])
+            
+        elif "101" == comp:
+            conv_data.append(descriptive[-3:] +[novel_normalized[i-3], novel_normalized[i-1]])
+
+        elif "111" == comp:
+            conv_data.append(descriptive[-3:] + novel_normalized[i-2:i])
+
+        else:
+            pass
+    
+    return conv_data
+
+
+conv_data = []
+from tqdm import tqdm
+for ncode in tqdm(ncodes):
+    for number in sorted(os.listdir(novel_path+ncode) ,reverse=True):
+        with open(novel_path+ncode+"/"+number, "r") as f:
+            novel = f.read().splitlines()
+            conv_data += novel2data(novel) 
+
+out_path = "../../corpus/novel_formated/"
+
+corpus_name = "novel_segments.tsv"
+import csv
+
+with open(out_path+corpus_name, "w") as f:
+    writer = csv.writer(f, delimiter="\t")
+    writer.writerows(conv_data)
---
+++ train/data_process/novel_collect_v2.py
@@ -0,0 +1,148 @@
+import requests
+import yaml
+import json
+import gzip
+import pandas as pd
+
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+import os
+from tqdm import tqdm
+
+
+def get_one_page(url):
+    # url = "http://www.yomiuri.co.jp/"
+    text_list = []
+    try:
+        html = request.urlopen(url)
+
+        #set BueatifulSoup
+        soup = BeautifulSoup(html, "html.parser")
+
+        #get 本文
+        contents = soup.find("div", id="novel_honbun", class_="novel_view")
+        contents_p = contents.find_all("p")
+        
+        for text in contents_p:
+            # line = re.sub("<.+>", "", str(line))
+            text = text.get_text().strip()
+            if not text :
+                continue
+            text_list.append(text)
+
+        return text_list
+        
+    except Exception as E:
+        return text_list
+
+base_url = "https://ncode.syosetu.com/"
+def crawl_one_novel_div_page(ncode:str, start=1, end=10):
+    novel_url = base_url+ncode.lower()+"/"
+    novel = []
+    for i in range(start, end+1) :
+        url = novel_url+str(i)
+        page = get_one_page(url)
+        if page==[]:
+            break
+        novel.append(page)
+    return novel
+
+
+class Collector2:
+    def __init__(self, out_path="../../corpus/novel2/"):
+        self.out_path = out_path
+        self.utt_set = set()
+        self.title_set = set()
+    
+    def crawl(self, limit=5000, num_page=100):
+        # limit 以下の場合に実行
+        
+        # 小説タイトルが沢山
+        ncodes = self.get_titles(limit)
+        for i, ncode in enumerate(tqdm(ncodes)):
+            
+            # タイトルが既に登録されていれば無視
+            if ncode in self.title_set:
+                continue
+            # print(ncode)
+            # 会話文のみ
+            contents = self.extract_contents_from_ncode(ncode, end=num_page)
+            # self.utt_set.update(set(convs))
+            # 小説の保存
+            self.write(ncode, contents)
+                # 追加したタイトルを保存
+            self.title_set.add(ncode)
+
+            # 進捗
+            if (i+1) % 10 == 0:
+                print("complete rate => {0} : {1}%".format(i+1, (i+1)*100//len(ncodes)))
+        
+        print("ended crawl/ crawled data => {0} : {1}%".format(i+1, (i+1)*100//len(ncodes)))
+            
+    
+    def extract_contents_from_ncode(self, ncode, end=100) -> list:
+        novel = crawl_one_novel_div_page(ncode, end=end)
+        return novel
+    
+    def get_titles(self, limit):
+        payload = {'out': 'json','gzip':5,'order':'yearlypoint','lim':limit}
+        res = requests.get("https://api.syosetu.com/novelapi/api/?genre=102&length=100000-&type=r", params=payload, timeout=30).content
+        r =  gzip.decompress(res).decode("utf-8")
+        df_temp = pd.read_json(r)
+        df_temp = df_temp.drop(0)
+        ncodes = list(df_temp.ncode.values)
+        return ncodes
+    
+    def save(self, filename, titles):
+        # conv_data = dict()
+        # conv_data["utt"] = sorted( list(self.utt_set) ) 
+
+        title_data = dict()
+        title_data["ncode"] = sorted( list(self.title_set) ) 
+
+        # with open(self.out_path+filename, mode="w") as f:
+        #     json.dump(conv_data, f, ensure_ascii=False, indent=4)
+        
+        with open(self.out_path+titles, mode="w") as f:
+            json.dump(title_data, f, ensure_ascii=False, indent=4)
+        
+    def load(self, filename, titles):
+        # 実データ
+        # if not os.path.exists(self.out_path+filename):
+        #     print("file was not found : {0}".format(self.out_path+filename))
+        #     return False
+        # else:
+        #     # jsonファイル
+        #     with open(self.out_path+filename, mode="r") as f:
+        #         data = json.load(f)
+        #     self.utt_set = set(data["utt"])
+        #     print("success load : {0}".format(self.out_path+filename))
+        
+        # 読み込んだタイトル達
+        if not os.path.exists(self.out_path+titles):
+            print("file was not found : {0}".format(self.out_path+titles))
+            return False
+
+        else:
+            with open(self.out_path+titles, mode="r") as f:
+                data = json.load(f)
+            self.title_set = set(data["ncode"])
+            print("success load : {0}".format(self.out_path+titles))
+            self.title_set.update(set(os.listdir(self.out_path)))
+        
+        return True
+
+    def write(self, ncode, contents):
+        os.makedirs(self.out_path+ncode, exist_ok=True)
+        for i, content in enumerate(contents):
+            # print(content)
+            with open(self.out_path+ncode+"/"+"{0}.txt".format(i+1), "w") as f:
+                f.write("\n".join(content))
+
+
+titles = "ncodes_{0}.json".format(50)
+col = Collector2(out_path="../../corpus/novel/")
+col.load("", titles)
+col.crawl(limit=100, num_page=100)
+col.save("", titles)
